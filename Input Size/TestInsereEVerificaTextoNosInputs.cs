8c0cefcf9d015a87de38c7605076d44e6687160b
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2020-09-05 19:00:32 -0300

    Implementando o primeiro teste do Input Size.

---
+++ Input Size/TestInsereEVerificaTextoNosInputs.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using TesteAutomatizadoBootStrapDash.Page_Object;
+using TesteAutomatizadoBootStrapDash.Utils;
+
+namespace TesteAutomatizadoBootStrapDash.Input_Size
+{
+    [TestFixture]
+    public class TestInsereEVerificaTextoNosInputs : BaseTest
+    {
+        [Test]
+        public void InsereEVerificaTextoNosInputs()
+        {
+            AbreUrl("https://www.bootstrapdash.com/demo/star-admin-pro-vue/demo_1/#/basicFormElements/");
+            InputSizeElementsPgo pageObject = new InputSizeElementsPgo(driver);
+
+            Esperas espera = new Esperas(driver);
+                espera.EsperaElementoSerClicavel(pageObject.CampoSmallInput);
+
+                    DadosParaInputs dados = new DadosParaInputs();
+
+                        pageObject.CampoLargeInput.SendKeys(dados.Email);
+
+                        pageObject.CampoDefaultInput.SendKeys(dados.Email);
+
+                        pageObject.CampoSmallInput.SendKeys(dados.Email);
+
+                        pageObject.ComboSmallInput.Click();
+                            pageObject.OptionSmallInput.Click();
+
+                        pageObject.ComboDefaultInput.Click();
+                            pageObject.OptionDefaultInput.Click();
+
+                        pageObject.CampoLargeInput.Click();
+                            pageObject.OptionLargeInput.Click();
+
+            Assert.AreEqual(dados.Email, BuscaValorDoElemento(driver, pageObject.CampoLargeInput));
+
+            Assert.AreEqual(dados.Email, BuscaValorDoElemento(driver, pageObject.CampoDefaultInput));
+
+            Assert.AreEqual(dados.Email, BuscaValorDoElemento(driver, pageObject.CampoSmallInput));
+
+            Assert.AreEqual("This a first option", pageObject.ComboSmallInput.Text);
+
+            Assert.AreEqual("Selected Option", BuscaValorDoElemento(driver, pageObject.ComboDefaultInput));
+
+            Assert.AreEqual("This is an option with object value", BuscaValorDoElemento(driver, pageObject.ComboLargeInput));
+
+            var select = document.getElementById('__BVID__152').option = select.options[select.selectedIndex];
+
+
+        }
+    }
+}
---
+++ Page Object/InputSizeElementsPgo.cs
@@ -0,0 +1,73 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteAutomatizadoBootStrapDash.Page_Object
+{
+    class InputSizeElementsPgo
+    {
+        public IWebElement TituloCard { get; set; }
+        public IWebElement DescricaoCard { get; set; }
+        public IWebElement LabelLargeInput { get; set; }
+        public IWebElement CampoLargeInput { get; set; }
+        public IWebElement LabelDefaultInput { get; set; }
+        public IWebElement CampoDefaultInput { get; set; }
+        public IWebElement LabelSmallInput { get; set; }
+        public IWebElement CampoSmallInput { get; set; }
+        public IWebElement SubTituloCard { get; set; }
+        public IWebElement ComboSmallInput { get; set; }
+        public IWebElement ComboDefaultInput { get; set; }
+        public IWebElement ComboLargeInput { get; set; }
+        public IWebElement OptionSmallInput { get; set; }
+        public IWebElement OptionDefaultInput { get; set; }
+        public IWebElement OptionLargeInput { get; set; }
+
+
+        public InputSizeElementsPgo(IWebDriver driver)
+        {
+
+
+            
+            TituloCard = driver.FindElement(By.CssSelector("div.col-md-7.grid-margin.stretch-card > div > div:nth-child(1) > h4"));
+            
+            DescricaoCard = driver.FindElement(By.CssSelector("div.col-md-7.grid-margin.stretch-card > div > div:nth-child(1) > p"));
+            
+            LabelLargeInput = driver.FindElement(By.Id("__BVID__141__BV_label_"));
+            
+            CampoLargeInput = driver.FindElement(By.CssSelector("#__BVID__141  input"));
+            
+            LabelDefaultInput = driver.FindElement(By.Id("__BVID__143__BV_label_"));
+            
+            CampoDefaultInput = driver.FindElement(By.CssSelector("#__BVID__143 input"));
+            
+            LabelSmallInput = driver.FindElement(By.Id("__BVID__145__BV_label_"));
+            
+            CampoSmallInput = driver.FindElement(By.CssSelector("#__BVID__145 input"));
+            
+            SubTituloCard = driver.FindElement(By.CssSelector("div:nth-child(2) > h4"));
+            
+            ComboSmallInput = driver.FindElement(By.Id("__BVID__148"));
+
+            OptionSmallInput = driver.FindElement(By.CssSelector("#__BVID__148 option[value='a']"));
+            
+            ComboDefaultInput = driver.FindElement(By.Id("__BVID__150"));
+
+            OptionDefaultInput = driver.FindElement(By.CssSelector("#__BVID__150 option[value='b']"));
+            
+            ComboLargeInput = driver.FindElement(By.Id("__BVID__152"));
+
+            OptionLargeInput = driver.FindElement(By.CssSelector("#__BVID__152 option[value='[object Object]']"));
+
+
+        }
+    }
+
+        
+        
+
+
+    
+}
